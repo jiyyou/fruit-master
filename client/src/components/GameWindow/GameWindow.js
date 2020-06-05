210f409a59790ea26236b54ceb2b69358ea3e52f
Jiyo <ji.yyou@gmail.com>
2020-06-05 10:13:49 -0400

    initialize folder structure

---
+++ client/src/components/GameWindow/GameWindow.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './GameWindow.scss';
+import axios from 'axios';
+
+class GameWindow extends React.Component {
+	state = {
+
+	}
+
+	render() {
+		return (
+
+		)
+	}
+}
